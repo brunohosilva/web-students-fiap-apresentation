8b7b16fe2573b1f2cae191ba5f722533f5a3311f
Bruno Oliveira <brunohosilva97@gmail.com>
2023-05-22 21:43:54 -0300

    :sparkles: created a profile description student

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
+import { students } from "./helper/data.ts";
+
+function renderImage(image) {
+  return (
+    <img
+      src={image}
+      alt="logo"
+      style={{ width: 100, borderRadius: 100, marginRight: 30 }}
+    />
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Accordion defaultActiveKey="0">
+      {students.data.map((student) => (
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>
+            {renderImage(student.image)}{" "}
+            <Card.Title>
+              {student.name}: RM {student.rm}
+            </Card.Title>
+          </Accordion.Header>
+          <Accordion.Body>
+            <Card.Title>Trabalho:</Card.Title>{" "}
+            <Card.Text>{student.job}</Card.Text>
+            <Card.Title>Sobre mim:</Card.Title>{" "}
+            <Card.Text>{student.about}</Card.Text>
+          </Accordion.Body>
+        </Accordion.Item>
+      ))}
+    </Accordion>
   );
 }
 
---
+++ src/helper/data.ts
@@ -0,0 +1,11 @@
+export const students = {
+    data: [
+        {
+            name: "Bruno",
+            job: "Desenvolvedor Mobile em Neon Pagamentos",
+            image: require('../assets/bruno-perfil.jpeg'),
+            about: "Atualmente trabalho com a tecnologia React native, em paralelo estudando Swift e atuando em algumas tarefas em iOS 🍎 nativo. Sou formado em Análise e desenvolvimento de sistemas pela FATEC Sjc e Pós graduado em Desenvolvimento Mobile pela FIAP. Sou desenvolvedor de software há 6 anos, 3 primeiros anos trabalhando com desenvolvimento voltado a web e backend, e nos últimos 3 anos migrei para a área de desenvolvimento mobile",
+            rm: "344458"
+        }
+    ]
+}
